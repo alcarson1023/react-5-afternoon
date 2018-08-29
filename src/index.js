1f98f853b2abd3923bc0e561b5649f41ea47f96e
Alexander Carson <alexandercarson@Alexanders-MacBook-Pro.local>
2018-08-29 15:57:39 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { HashRouter } from 'reaect-router-dom'
+import { Provider } from 'react-redux'
+import store from './store.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+<HashRouter><App /></HashRouter>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
 
